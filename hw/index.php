0f7ec2491cc5b9394af4970b2097decc6bd1ff73
Александр Гетман <alexander_getman@mail.ru>
2018-06-29 22:41:05 +1000

    hw2

--- hw/index.php
+++ hw/index.php
@@ -1,36 +1,20 @@
 <?php
+$x = rand(0,100);
+echo "Число $x<br>";
+$var1 = 1;
+$var2 = 1;
 
-$name = 'Александр';
-$age = 29;
-$address = 'Владивосток';
-$email = 'alexander_getman@mail.ru';
-$aboutme = 'Контент-менеджер, киноман, меломан'
+while (true) {
+    if ($var1 > $x) {
+        echo "задуманное число НЕ входит в числовой ряд<br>";
+        break;
+    } elseif ($var1 === $x) {
+        echo "задуманное число входит в числовой ряд<br>";
+        break;
+    }
+    $var3 = $var1;
+    $var1 += $var2;
+    $var2 = $var3;
+}
+
 ?>
-
-<!DOCTYPE>
-<html lang="ru">
-  <head>
-    <title><?= $name . ' – ' . $aboutme ?></title>
-    <meta charset="utf-8">
-    <style>
-        body {
-            font-family: sans-serif;
-        }
-    </style>
-  </head>
-  <body>
-    <h1>Страница пользователя <?= $name ?></h1>
-      <dl>
-        <dt>Имя</dt>
-        <dd><?= $name ?></dd>
-        <dt>Возраст</dt>
-        <dd><?= $age ?></dd>
-        <dt>Адрес электронной почты</dt>
-        <dd><a href="mailto:<?= $email ?>"><?= $email ?></a></dd>
-        <dt>Город</dt>
-        <dd><?= $address ?></dd>
-        <dt>О себе</dt>
-        <dd><?= $aboutme ?></dd>
-      </dl>
-  </body>
-</html>
